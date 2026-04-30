71e27afeb05200fa6da912eede20e4ef1775527a
tnaxkes <izhgrish@gmail.com>
2026-04-30 03:05:10 +0300

    set broadcast test time

--- bot/services/lead_broadcast_service.py
+++ bot/services/lead_broadcast_service.py
@@ -16,7 +16,7 @@ from config.settings import get_settings
 logger = logging.getLogger(__name__)
 
 BROADCAST_TIMES = (
-    time(hour=2, minute=40),
+    time(hour=3, minute=8),
     time(hour=23, minute=45),
 )
 
