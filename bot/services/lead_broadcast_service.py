5ad312ac03551b4ed4a47f868276814989eaa782
tnaxkes <izhgrish@gmail.com>
2026-04-30 12:49:24 +0300

    Move first broadcast to 12:55

--- bot/services/lead_broadcast_service.py
+++ bot/services/lead_broadcast_service.py
@@ -16,7 +16,7 @@ from config.settings import get_settings
 logger = logging.getLogger(__name__)
 
 BROADCAST_TIMES = (
-    time(hour=12, minute=46),
+    time(hour=12, minute=55),
     time(hour=23, minute=45),
 )
 
